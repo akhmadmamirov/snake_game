701daa3dae1711d847555581bf001ee999c19a73
Akhmadillo Mamirov <akhmadillomamirov@Akhmadillos-MacBook-Air.local>
2022-12-28 10:28:34 -0800

    updating files

--- main.py
+++ main.py
@@ -1,45 +1,52 @@
 from turtle import Screen
-import time
-from food import Food
 from snake import Snake
+from food import Food
 from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor('red')
+screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
 food = Food()
-score = Scoreboard()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    
-    #Detect collision with food
+
+    #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        score.update_score()
+        scoreboard.increase_score()
 
-    #Detect collisions with the wall
-    if snake.head.xcor() > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -300: 
-        game_is_on = False
-        score.game_over()
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
 
-    #Detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over( )
-    #if head collides with  any segment in the tail
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+           scoreboard.reset()
+           snake.reset()
+
+
+
+
+
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,22 +1,34 @@
 from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
 class Scoreboard(Turtle):
+
     def __init__(self):
         super().__init__()
+        with open("data.txt") as file:
+            self.highscore = int(file.read())
         self.score = 0
-        self.hideturtle()
+        self.color("white")
         self.penup()
-        self.goto(0,270)
-        self.color('white')
-        self.update_score()
-        
-        
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
 
-    def update_score(self):
+    def update_scoreboard(self):
         self.clear()
-        self.write(f'Score: {self.score}', move=False, align='center', font=('Arial', 24,'normal'))
-        self.score += 1
+        self.write(f"Score: {self.score} Highestscore: {self.highscore}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write('GAME OVER', move=False, align='center', font=('Arial', 24,'normal'))
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", "w") as file:
+                file.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
